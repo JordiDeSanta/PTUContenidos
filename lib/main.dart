23cbafd315b8090841411cac16c7d3631f97c38e
Jordi <jordi.bravo.bad@gmail.com>
2021-05-22 17:47:27 -0400

    Importing icons and folder hierarchy

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:pdf_render/pdf_render_widgets.dart';
+import 'package:ptucontenidos/pages/home_page.dart';
+import 'package:ptucontenidos/pages/pdf_page.dart';
 
 void main(List<String> args) => runApp(new MyApp());
 
@@ -12,65 +14,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final controller = PdfViewerController();
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: ValueListenableBuilder<Object>(
-              // The controller is compatible with ValueListenable<Matrix4> and you can receive notifications on scrolling and zooming of the view.
-              valueListenable: controller,
-              builder: (context, _, child) => Text(controller.isReady
-                  ? 'Page #${controller.currentPageNumber}'
-                  : 'Page -')),
-        ),
-        backgroundColor: Colors.grey,
-        body: Platform.isMacOS
-            // Networking sample using flutter_cache_manager
-            ? PdfViewer.openFutureFile(
-                // Accepting function that returns Future<String> of PDF file path
-                () async => (await DefaultCacheManager().getSingleFile(
-                        'https://github.com/espresso3389/flutter_pdf_render/raw/master/example/assets/hello.pdf'))
-                    .path,
-                viewerController: controller,
-                onError: (err) => print(err),
-                params: PdfViewerParams(
-                  padding: 10,
-                  minScale: 1.0,
-                ),
-              )
-            : PdfViewer.openAsset(
-                'assets/Biology/pauta.pdf',
-                viewerController: controller,
-                onError: (err) => print(err),
-                params: PdfViewerParams(
-                  padding: 10,
-                  minScale: 1.0,
-                ),
-              ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              child: Icon(Icons.first_page),
-              onPressed: () => controller.ready?.goToPage(pageNumber: 1),
-            ),
-            FloatingActionButton(
-              child: Icon(Icons.last_page),
-              onPressed: () =>
-                  controller.ready?.goToPage(pageNumber: controller.pageCount),
-            ),
-          ],
-        ),
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
       ),
+    );
+
+    return MaterialApp(
+      title: 'PTU Formulas',
+      initialRoute: 'home',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+        'pdf': (BuildContext context) => PDFPage(),
+      },
+      theme: ThemeData(accentColor: Colors.white),
     );
   }
 }
