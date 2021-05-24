63f9cf8d2df1210ac787c5b9e86bced62c3ce382
Jordi <jordi.bravo.bad@gmail.com>
2021-05-24 19:39:13 -0400

    Course Page sketch

--- lib/pages/course_page.dart
+++ lib/pages/course_page.dart
@@ -20,6 +20,41 @@ class CoursePage extends StatelessWidget {
         toolbarHeight: size * 140,
         backgroundColor: args.courseColor,
       ),
+      body: Table(
+        children: [
+          TableRow(
+            children: [
+              _createButton(),
+              _createButton(),
+            ],
+          ),
+          TableRow(
+            children: [
+              _createButton(),
+              _createButton(),
+            ],
+          )
+        ],
+      ),
     );
   }
+}
+
+Widget _createButton() {
+  return Container(
+    padding: EdgeInsets.all(10),
+    height: 350,
+    child: ElevatedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        elevation: MaterialStateProperty.all(0.0),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+      ),
+      child: null,
+    ),
+  );
 }
